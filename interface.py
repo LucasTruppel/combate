a4306ea754e48c470daddb44646abe7d56f67136
LucasTruppel <lucasctruppelm@gmail.com>
2023-03-29 11:27:41 -0300

    Compartimentos laterais de peças desenhados no tkinter

--- interface.py
+++ interface.py
@@ -10,18 +10,18 @@ class InterfaceGraficaJogador:
         self.janela_principal.resizable(False, False)
         self.janela_principal.configure(bg="gray")
         
-        
         self.lado_borda_frame_principal = int(ALTURA*0.85)
         self.lado_frame_principal = int(self.lado_borda_frame_principal*0.98)
+        self.lado_celula = self.lado_frame_principal//10 - int(0.0030*self.lado_frame_principal)
 
-        
+
         self.borda_frame_principal = Frame(self.janela_principal,
                                            bg="black")
         self.borda_frame_principal.place(anchor="center",
                                    width=int(self.lado_borda_frame_principal), 
                                    height=int(self.lado_borda_frame_principal),
                                    x = LARGURA//2,
-                                   y = ALTURA//2)
+                                   y = ALTURA//2 + ALTURA//20)
         
         self.frame_principal = Frame(self.janela_principal,
                                      bg="black")
@@ -29,10 +29,8 @@ class InterfaceGraficaJogador:
                                    width=self.lado_frame_principal, 
                                    height=self.lado_frame_principal,
                                    x = LARGURA//2,
-                                   y = ALTURA//2)
-        
-        self.lado_celula = self.lado_frame_principal//10 - int(0.0030*self.lado_frame_principal)
-        
+                                   y = ALTURA//2 + ALTURA//20)
+                
         for i in range(10):
             for j in range(10):
                 
@@ -52,7 +50,88 @@ class InterfaceGraficaJogador:
                                     anchor="center"
                                     )
         
+        self.borda_frame_pecas_direita = Frame(self.janela_principal,
+                                               bg="black")
+        self.borda_frame_pecas_direita.place(anchor="e", 
+                                             width=2*self.lado_celula + int(0.0125*self.lado_borda_frame_principal), 
+                                             height=int(0.6*self.lado_borda_frame_principal + 0.0125*self.lado_borda_frame_principal),
+                                             x = LARGURA//2 - self.lado_borda_frame_principal//2,
+                                             y = ALTURA//2 + ALTURA//20)
+        
+        self.frame_pecas_direita = Frame(self.janela_principal,
+                                      bg="black")
+        self.frame_pecas_direita.place(anchor="e",
+                                    width=2*self.lado_celula, 
+                                    height=int(0.6*self.lado_frame_principal),
+                                    x = LARGURA//2 - self.lado_borda_frame_principal//2 - int(0.02*self.lado_celula),
+                                    y = ALTURA//2 + ALTURA//20)
+        
+        for i in range(6):
+            for j in range(2):
+                label_posicao = Label(self.frame_pecas_direita,
+                                      bg="white")
+                label_posicao.place(x=j*self.lado_celula,
+                                    y=i*self.lado_frame_principal//10,
+                                    width=self.lado_celula, 
+                                    height=self.lado_celula,
+                                    anchor="nw"
+                                    )
+                
+        
+        
+        self.borda_frame_pecas_esquerda = Frame(self.janela_principal,
+                                               bg="black")
+        self.borda_frame_pecas_esquerda.place(anchor="w", 
+                                             width=2*self.lado_celula + int(0.0125*self.lado_borda_frame_principal), 
+                                             height=int(0.6*self.lado_borda_frame_principal + 0.0125*self.lado_borda_frame_principal),
+                                             x = LARGURA//2 + self.lado_borda_frame_principal//2,
+                                             y = ALTURA//2 + ALTURA//20)
+        
+        self.frame_pecas_esquerda = Frame(self.janela_principal,
+                                      bg="black")
+        self.frame_pecas_esquerda.place(anchor="w",
+                                    width=2*self.lado_celula, 
+                                    height=int(0.6*self.lado_frame_principal),
+                                    x = LARGURA//2 + self.lado_borda_frame_principal//2 + int(0.02*self.lado_celula),
+                                    y = ALTURA//2 + ALTURA//20)
+        
+        for i in range(6):
+            for j in range(2):
+                label_posicao = Label(self.frame_pecas_esquerda,
+                                      bg="white")
+                label_posicao.place(x=j*self.lado_celula,
+                                    y=i*self.lado_frame_principal//10,
+                                    width=self.lado_celula, 
+                                    height=self.lado_celula,
+                                    anchor="nw"
+                                    )
+                
+        
+        mensagem = "Seu turno!"
+        self.frameMensagem = Frame(self.janela_principal,
+                                   bg="gray"
+                                   )
+        self.frameMensagem.place(anchor="n", 
+                                 width=int(self.lado_borda_frame_principal), 
+                                 height=int(self.lado_borda_frame_principal//8),
+                                 x = LARGURA//2,
+                                 y = int(0.01*ALTURA))
+        
+        self.labelMensagem = Label(self.frameMensagem,
+                                   bg="gray",
+                                   text=mensagem,
+                                   font="arial 30"
+                                   )
+        self.labelMensagem.place(anchor="nw",
+                                 width=int(self.lado_borda_frame_principal), 
+                                 height=int(self.lado_borda_frame_principal//8),
+                                 x = 0,
+                                 y = 0
+                                 )
+        
+        
         self.janela_principal.mainloop()
+        
 
 
 
